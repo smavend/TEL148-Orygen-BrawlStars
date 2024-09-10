c4c31775e6c8105598347cc025dad904d6ca651b
smavend <amanrique068@gmail.com>
2024-09-09 21:05:53 -0500

    first commit

---
+++ src/test/java/com/example/webapporygen/WebAppOrygenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webapporygen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppOrygenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
